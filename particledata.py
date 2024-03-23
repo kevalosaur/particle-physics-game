6de58a4227ef1159804dd1ea1e862c6eb51ce329
greenturtle1134 <daniel.y.fu@gmail.com>
2024-03-23 18:35:30 -0500

    Changed the gamma character back to a y
    Thanks but no thanks ChatGPT

--- particledata.py
+++ particledata.py
@@ -30,4 +30,4 @@ class DecayMode:
 
 ELECTRON = ParticleType("e-", 1, -1, False, None, [])
 POSITRON = ParticleType("e+", 1, +1, False, None, [])
-PHOTON = ParticleType("γ", 0, 0, False, None, [])
+PHOTON = ParticleType("y", 0, 0, False, None, [])
