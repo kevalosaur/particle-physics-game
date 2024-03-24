92014916b91253d2f814bd6eff2d42c9efa31d61
greenturtle1134 <daniel.y.fu@gmail.com>
2024-03-24 03:17:20 -0500

    Changed over to YAML
    At least, I think it's working

--- loadbuttons.py
+++ loadbuttons.py
@@ -4,4 +4,5 @@ from pyscript import document
 buttondiv = document.querySelector('#buttons')
 
 for particle in particledata.PARTICLES:
+    particle = particledata.PARTICLES[particle]
     buttondiv.innerHTML += f'<button id={particle.ident}>{particle.symbol}</button>'
