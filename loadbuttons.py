89ef3ce0acdaf669deb3844a9d7278375f7168d6
kevalosaur <kddchess@yahoo.com>
2024-03-24 04:29:23 -0500

    loading wheel

--- loadbuttons.py
+++ loadbuttons.py
@@ -2,8 +2,11 @@ import particledata
 from pyscript import document
 
 buttondiv = document.querySelector('#buttons')
+buttonHTML = ""
 
 for particle in particledata.PARTICLES:
     particle = particledata.PARTICLES[particle]
     put_bar = " antimatter-bar" if particle.ident in ['ve_bar', 'vmu_bar'] else ""
-    buttondiv.innerHTML += f'<button class="particle-button{put_bar}" id={particle.ident}>{particle.symbol}<span class="particle-tooltip">{particle.desc_text()}</span></button>'
+    buttonHTML += f'<button class="particle-button{put_bar}" id={particle.ident}>{particle.symbol}<span class="particle-tooltip">{particle.desc_text()}</span></button>'
+
+buttondiv.innerHTML = buttonHTML
