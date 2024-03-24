230ecedb7987d062269374e3fc6b8e16b3ae16d3
kevalosaur <kddchess@yahoo.com>
2024-03-24 04:15:36 -0500

    button formatting for neutrinos

--- loadbuttons.py
+++ loadbuttons.py
@@ -5,4 +5,5 @@ buttondiv = document.querySelector('#buttons')
 
 for particle in particledata.PARTICLES:
     particle = particledata.PARTICLES[particle]
-    buttondiv.innerHTML += f'<button class="particle-button" id={particle.ident}>{particle.symbol}<span class="particle-tooltip">{particle.desc_text()}</span></button>'
+    put_bar = " antimatter-bar" if particle.ident in ['ve_bar', 'vmu_bar'] else ""
+    buttondiv.innerHTML += f'<button class="particle-button{put_bar}" id={particle.ident}>{particle.symbol}<span class="particle-tooltip">{particle.desc_text()}</span></button>'
